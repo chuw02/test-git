7056d5d7e91342313f2c3f4c781d00c169b75871
chuw paul <chuw30292@gmail.com>
2020-03-17 01:25:10 +0100

    project init

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+$age = 8;
+$langue = "français";
+
+
+if ($age <= 12 AND $langue == "français")
+{
+    echo "Bienvenue sur mon site !<br/>";
+}
+elseif ($age <= 12 AND $langue == "anglais")
+{
+    echo "Welcome to my website!<br/>";
+}
+
+$note = 16;
+
+if ($note == 0)
+{
+    echo "Tu es vraiment un gros nul !!!";
+}
+
+elseif ($note == 5)
+{
+    echo "Tu es très mauvais";
+}
+
+elseif ($note == 7)
+{
+    echo "Tu es mauvais";
+}
+
+elseif ($note == 10)
+{
+    echo "Tu as pile poil la moyenne, c'est un peu juste…";
+}
+
+elseif ($note == 12)
+{
+    echo "Tu es assez bon";
+}
+
+elseif ($note == 16)
+{
+    echo "Tu te débrouilles très bien !";
+}
+
+elseif ($note == 20)
+{
+    echo "Excellent travail, c'est parfait !";
+}
+
+else
+{
+    echo "Désolé, je n'ai pas de message à afficher pour cette note";
+}
+
+?>
